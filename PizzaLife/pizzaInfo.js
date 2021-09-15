f51d7dd8672d5627bd8ab2063f83f7e08435379f
yiliang0303 <57569150+yiliang0303@users.noreply.github.com>
2021-09-15 18:47:18 +0800

    Add files via upload

---
+++ PizzaLife/pizzaInfo.js
@@ -0,0 +1,18 @@
+﻿function resize(a)
+{	
+	if(a == 1)
+	{	
+		document.getElementById('viewMenu').style.height =  "620px";
+		document.getElementById('pizzaInfo').style.display = "none";
+	}
+	else
+	{
+		document.getElementById('viewMenu').style.height = "1250px";
+		document.getElementById('pizzaInfo').style.display = "none";
+		
+		if(a==3)
+		{
+			document.getElementById('pizzaInfo').style.display = "block";
+		}
+	}
+}
---
+++ PizzaLife/sound-mouseover.js
@@ -0,0 +1,15 @@
+﻿function playclip() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "scripts/sound-mouseover/click.wav";
+ }
+}
+
+else {
+{
+var audio = document.getElementsByTagName("audio")[0];
+audio.play();
+}
+}
+}
